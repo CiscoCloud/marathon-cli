397a7e5163392d13dc15614293142b431e9eed1c
Steven Borrelli <steve@aster.is>
2015-02-13 15:56:13 -0600

    Refactor out commands from main

--- marathon-cli.go
+++ marathon-cli.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	log "github.com/Sirupsen/logrus"
+	//log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
-	"github.com/jbdalido/go-marathon"
-	"net/http"
 	"os"
 )
 
@@ -14,39 +11,6 @@ const (
 	AUTHOR  = "Steven Borrelli"
 	EMAIL   = "steve@aster.is"
 )
-
-func LsApps(c *cli.Context) {
-	m := MarathonClient(c.GlobalString("host"))
-
-	if len(c.Args()) > 0 {
-		for _, app := range c.Args() {
-			r, err := m.GetApp(app)
-			if err != nil {
-				log.Error("App not found: ", app)
-			} else {
-				PrettyJson(r)
-			}
-		}
-	} else {
-		r, err := m.ListApps()
-
-		if err != nil {
-			log.Fatal("Error listing apps ", err)
-		}
-		PrettyJson(r)
-	}
-}
-
-func Ping(c *cli.Context) {
-	url := fmt.Sprintf("http://%s%s/ping", c.GlobalString("host"), gomarathon.APIVersion)
-
-	_, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error contacting marathon url: %s", err)
-	}
-
-	log.Info("Ping successful: ", url)
-}
 
 func run_cli() {
 	app := cli.NewApp()
