4a28d1f274911644f16b2acaa360972256980ff3
Steven Borrelli <steve@aster.is>
2015-02-15 20:42:51 -0600

    Add output functions

--- util.go
+++ util.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/asteris-llc/gomarathon"
 )
+
+func Output(format string, r *gomarathon.Response, fields ...string) {
+	switch format {
+	case "json", "JSON":
+		OutputJson(r)
+	default:
+		log.Fatal(fmt.Sprintf("Output format '%s' is not supported", format))
+	}
+}
+
+func OutputJson(r *gomarathon.Response) {
+	v, _ := json.MarshalIndent(r, "", "   ")
+	fmt.Printf("%s\n", v)
+}
 
 func PrettyJson(r interface{}) {
 	v, _ := json.MarshalIndent(r, "", "    ")
-	log.Info("%s", v)
+	log.Info(string(v))
 }
