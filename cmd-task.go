b48cc547485094ab1eade7ac8f41e1c1aca70566
Steven Borrelli <steve@aster.is>
2015-02-20 18:38:42 -0600

    Add func to kill all the apps that belong to a task

--- cmd-task.go
+++ cmd-task.go
@@ -2,6 +2,7 @@ package main
 
 // Commands dealing with tasks
 import (
+	"errors"
 	log "github.com/Sirupsen/logrus"
 	"github.com/asteris-llc/gomarathon"
 	"github.com/codegangsta/cli"
@@ -25,5 +26,28 @@ func LsTask(c *cli.Context) (*gomarathon.Response, error) {
 		log.Error("Error fetching tasks: ", err)
 		return nil, err
 	}
+	return resp, err
+}
+
+// Kill all tasks belonging to an app
+// The app will not be deleted
+func KillAppTasks(c *cli.Context) (*gomarathon.Response, error) {
+
+	m, _ := MarathonClient(c.GlobalString("host"))
+
+	var resp *gomarathon.Response
+	var err error
+
+	if len(c.Args()) > 0 {
+		resp, err = m.KillTasks(c.Args().First(), "", false)
+	} else {
+		log.Error("Please provide an app ID")
+		return nil, errors.New("No app ID provided")
+	}
+
+	if err != nil {
+		log.Error("Error killing tasks: ", err)
+	}
+
 	return resp, err
 }
